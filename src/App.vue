4f6d565b255381daa93066aba8fb0c05baa78a94
sunyan <sunyan@joygames.cc>
2023-10-15 23:51:52 +0800

    feat: init

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
-  <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
+  <!-- <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200"> -->
+  <main font-sans text=" gray-700 dark:gray-200" class="app-background">
     <RouterView />
-    <TheFooter />
+    <!-- <TheFooter /> -->
   </main>
 </template>
+
+<style>
+.app-background {
+  background-image: url('https://ug-drcn.dbankcdn.cn/nsp-campaign-res-drcn/campaignpreview/material/fa949bbdb13e4e6794b2525946db38d4/c3bf0c0c-1979-4a18-aa81-e2473472bc5f.PNG');
+  background-size: cover;
+  background-repeat: no-repeat;
+  /* 其他样式属性，如背景颜色等 */
+  min-height: 100vh;
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -13,34 +13,114 @@ function go() {
 </script>
 
 <template>
-  <div>
-    <div i-carbon-campsite inline-block text-4xl />
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
-        Vitesse Lite
-      </a>
-    </p>
-    <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
-    </p>
-
-    <div py-4 />
-
-    <TheInput
-      v-model="name"
-      placeholder="What's your name?"
-      autocomplete="false"
-      @keydown.enter="go"
-    />
-
-    <div>
-      <button
-        class="m-3 text-sm btn"
-        :disabled="!name"
-        @click="go"
-      >
-        Go
-      </button>
+  <div class="bg">
+    <div class="bg-cover">
+      <!-- <img
+        src="https://ug-drcn.dbankcdn.cn/nsp-campaign-res-drcn/campaignpreview/material/fa949bbdb13e4e6794b2525946db38d4/c3bf0c0c-1979-4a18-aa81-e2473472bc5f.PNG"
+        alt=""> -->
+    </div>
+    <div class="orrignInput">
+      <div i-carbon-campsite inline-block text-4xl />
+      <div py-4 />
+      <TheInput v-model="name" placeholder="What's your name?" autocomplete="false" @keydown.enter="go" />
+      <div>
+        <button class="m-3 text-sm btn" :disabled="!name" @click="go">
+          Go
+        </button>
+      </div>
+    </div>
+    <div class="orrignInput">
+      <div i-carbon-campsite inline-block text-4xl />
+      <div py-4 />
+      <TheInput v-model="name" placeholder="What's your name?" autocomplete="false" @keydown.enter="go" />
+      <div>
+        <button class="m-3 text-sm btn" :disabled="!name" @click="go">
+          Go
+        </button>
+      </div>
+    </div>
+    <div class="orrignInput">
+      <div i-carbon-campsite inline-block text-4xl />
+      <div py-4 />
+      <TheInput v-model="name" placeholder="What's your name?" autocomplete="false" @keydown.enter="go" />
+      <div>
+        <button class="m-3 text-sm btn" :disabled="!name" @click="go">
+          Go
+        </button>
+      </div>
+    </div>
+    <div class="orrignInput">
+      <div i-carbon-campsite inline-block text-4xl />
+      <div py-4 />
+      <TheInput v-model="name" placeholder="What's your name?" autocomplete="false" @keydown.enter="go" />
+      <div>
+        <button class="m-3 text-sm btn" :disabled="!name" @click="go">
+          Go
+        </button>
+      </div>
+    </div>
+    <div class="orrignInput">
+      <div i-carbon-campsite inline-block text-4xl />
+      <div py-4 />
+      <TheInput v-model="name" placeholder="What's your name?" autocomplete="false" @keydown.enter="go" />
+      <div>
+        <button class="m-3 text-sm btn" :disabled="!name" @click="go">
+          Go
+        </button>
+      </div>
+    </div>
+    <div class="orrignInput">
+      <div i-carbon-campsite inline-block text-4xl />
+      <div py-4 />
+      <TheInput v-model="name" placeholder="What's your name?" autocomplete="false" @keydown.enter="go" />
+      <div>
+        <button class="m-3 text-sm btn" :disabled="!name" @click="go">
+          Go
+        </button>
+      </div>
+    </div>
+    <div class="orrignInput">
+      <div i-carbon-campsite inline-block text-4xl />
+      <div py-4 />
+      <TheInput v-model="name" placeholder="What's your name?" autocomplete="false" @keydown.enter="go" />
+      <div>
+        <button class="m-3 text-sm btn" :disabled="!name" @click="go">
+          Go
+        </button>
+      </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.bg-cover {
+  position: absolute;
+  overflow: hidden;
+  z-index: 0;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+}
+
+.bg {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  opacity: 1;
+  background-image: url("https://ug-drcn.dbankcdn.cn/nsp-campaign-res-drcn/campaignpreview/material/fa949bbdb13e4e6794b2525946db38d4/c3bf0c0c-1979-4a18-aa81-e2473472bc5f.PNG");
+  background-size: 100%;
+  background-repeat: no-repeat;
+  background-clip: content-box;
+}
+
+.orrignInput {
+  /* position: absolute;
+  bottom: 10%;
+  left: 50%;
+  transform: translate(-50%, -50%); */
+  /* 透明度 */
+  opacity: 0.2;
+  background-color: antiquewhite;
+}
+</style>
